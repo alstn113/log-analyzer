cacc16b5c255ed5af67ed5b1a98692e82299ee71
alstn113 <alstn113@gmail.com>
2026-02-02 11:06:18 +0900

    feat: LogParser 인터페이스 작성

---
+++ src/main/java/io/github/alstn113/assignment/application/LogParser.java
@@ -0,0 +1,14 @@
+package io.github.alstn113.assignment.application;
+
+import io.github.alstn113.assignment.domain.analysis.vo.LogEntry;
+import io.github.alstn113.assignment.domain.analysis.vo.ParsingErrors;
+import java.io.File;
+import java.util.List;
+
+public interface LogParser {
+
+    ParseResult parse(File file);
+
+    record ParseResult(List<LogEntry> entries, ParsingErrors parsingErrors) {
+    }
+}
