e045938691e157030f7e32f6de23c2d86db4c5c7
alstn113 <alstn113@gmail.com>
2026-02-02 11:08:05 +0900

    feat: AnalysisRepository 인터페이스 작성

---
+++ src/main/java/io/github/alstn113/assignment/application/AnalysisRepository.java
@@ -0,0 +1,13 @@
+package io.github.alstn113.assignment.application;
+
+import io.github.alstn113.assignment.domain.analysis.Analysis;
+import java.util.Optional;
+
+public interface AnalysisRepository {
+
+    Analysis save(Analysis analysis);
+
+    void update(Analysis analysis);
+
+    Optional<Analysis> findById(Long id);
+}
