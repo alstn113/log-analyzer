6f43bd3192959f226a650b589593db610f4167a9
alstn113 <alstn113@gmail.com>
2026-02-02 15:12:39 +0900

    feat: FileStorage 인터페이스를 구현한 LocalTempFileStorage 구현

--- src/main/java/io/github/alstn113/assignment/infra/file/LocalTempFileStorage.java
+++ src/main/java/io/github/alstn113/assignment/infra/file/LocalTempFileStorage.java
@@ -1,20 +1,41 @@
 package io.github.alstn113.assignment.infra.file;
 
 import io.github.alstn113.assignment.application.FileStorage;
+import io.github.alstn113.assignment.application.exception.FileProcessingException;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Component
 public class LocalTempFileStorage implements FileStorage {
 
     @Override
     public File save(MultipartFile file) {
-        return null;
+        try {
+            String tempFileName = "upload-" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+            File tempFile = File.createTempFile(tempFileName, null);
+            file.transferTo(tempFile);
+            return tempFile;
+        } catch (IOException e) {
+            throw new FileProcessingException("Failed to save temporary file: " + e.getMessage(), e);
+        }
     }
 
     @Override
     public void delete(File file) {
+        if (file == null) {
+            return;
+        }
 
+        try {
+            Files.deleteIfExists(file.toPath());
+        } catch (IOException e) {
+            log.warn("Failed to delete temporary file: {}", file.getAbsolutePath(), e);
+        }
     }
 }
